195b16c033a0fea01aad2a4792bd6573a4ba78e8
Faizalilham <faizalfalakh19@gmail.com>
2021-06-25 15:20:10 +0700

    first commit

--- app/src/main/java/com/example/finalprojectbatch2/FragmentRegist.kt
+++ app/src/main/java/com/example/finalprojectbatch2/FragmentRegist.kt
@@ -39,7 +39,7 @@ class FragmentRegist : Fragment() {
         val password = binding.ETPasswordSignUp.text.toString()
 
       if(binding.ETUsernameSignUp.text.isNullOrEmpty() && binding.ETPasswordSignUp.text.isNullOrEmpty()){
-          Toast.makeText(activity,"Please answer all the questions",Toast.LENGTH_SHORT).show()
+          Toast.makeText(activity,"Create Account failed, please fill in all data",Toast.LENGTH_SHORT).show()
       }else{
           APIService.APIEndPoint().SignUp(name, username, email, password).enqueue(object : Callback<SingleRespon<Model>>{
               override fun onResponse(
